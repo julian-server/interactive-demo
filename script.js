683c11784ee1c6ba45acfd7a7b4daa201583c440
julian-server <julianpinney@gmail.com>
2025-03-27 13:03:34 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const interactiveElement = document.getElementById('interactive-element');
-
-document.addEventListener('mousemove', (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
-
-    interactiveElement.style.left = x + 'px';
-    interactiveElement.style.top = y + 'px';
-});
